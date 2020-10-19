e31d653d2fddbe44d45ba78d5c805a3a66890bee
rrocha-s <71041915+rrocha-s@users.noreply.github.com>
2020-10-19 09:34:10 -0500

    Cambio

--- src/main/java/com/smartech/invoicing/model/RetailComplement.java
+++ src/main/java/com/smartech/invoicing/model/RetailComplement.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -17,6 +20,10 @@ public class RetailComplement implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 	
 	@Column(name = "documentStatus", nullable = true)
     private String documentStatus;
